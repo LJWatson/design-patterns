3b203842734854372fe131ae50ccac560adddb44
LJWatson <tink@tink.co.uk>
2014-12-07 16:50:41 +0000

    Added accessible button example.

---
+++ button-example2/js/button.js
@@ -0,0 +1,26 @@
+function toggleButton(event)
+{
+	var button = document.getElementById("button");
+	
+	if(button.getAttribute("aria-pressed") == "false")
+	{
+		button.setAttribute("aria-pressed", "true");
+	}
+	else
+	{
+		button.setAttribute("aria-pressed", "false");
+	}
+}
+
+// Check if the Space (key code 32) or enter (key code 13) have been pressed
+function handleBtnKeyUp(event)
+{
+	event = event || window.event;
+	
+	if ((event.keyCode === 32) || (event.keyCode === 13))
+	{
+		toggleButton2(event);
+	}
+}
+
+window.onload = init;
