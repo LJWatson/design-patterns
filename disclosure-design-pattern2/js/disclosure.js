cbb205c86820d1fbfeacbf192bfef4e70e9573e6
LJWatson <tink@tink.co.uk>
2014-12-07 22:10:17 +0000

    Added button based disclosure widget design pattern.

---
+++ disclosure-design-pattern2/js/disclosure.js
@@ -0,0 +1,18 @@
+function toggleContent(event)
+{
+	var button = document.getElementById("button");
+	var content = document.getElementById("content");
+
+    if(content.getAttribute("aria-hidden") == "true")
+	{
+		content.setAttribute("aria-hidden", "false");
+		button.setAttribute("aria-expanded", "true");
+	}
+	else
+	{
+		content.setAttribute("aria-hidden", "true");
+		button.setAttribute("aria-expanded", "false");
+	}
+}
+
+window.onload = init;
